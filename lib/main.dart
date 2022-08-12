4ddec6b844e80d74442e6ef0579bcfa8358c02e6
komalfogawat <komal.fogawat.hp@gmail.com>
2022-08-12 13:00:01 +0530

    New branch created

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class _TodoappState extends State<Todoapp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("TodoApp"),
+        title: Text("Todo App"),
       ),
     );
   }
